3b7d1013dffd7af5b0160a93d22c773370e1ccee
Ping-Lin Chang <ping-lin.chang@umbocv.com>
2016-04-30 02:25:16 +0800

    Update prepare_image.m

--- prepare_image.m
+++ prepare_image.m
@@ -1,14 +1,14 @@
 function crops_data = prepare_image(im)
 % ------------------------------------------------------------------------
 % caffe/matlab/+caffe/imagenet/ilsvrc_2012_mean.mat contains mean_data that
-% is already in W x H x C with BGR channels
+% is already in H x W x C with BGR channels
 d = load('ilsvrc_2012_mean.mat');
 mean_data = d.mean_data;
 IMAGE_DIM = 256;
 CROPPED_DIM = 224; % 224 for googLeNet , 227 for VGG and AlexNet
 
 % Convert an image returned by Matlab's imread to im_data in caffe's data
-% format: W x H x C with BGR channels
+% format: H x W x C with BGR channels
 im_data = im(:, :, [3, 2, 1]);  % permute channels from RGB to BGR
 im_data = permute(im_data, [2, 1, 3]);  % flip width and height
 im_data = single(im_data);  % convert from uint8 to single
