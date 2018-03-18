611e16498489c634129669f79f7235709405a9db
Bolei Zhou <zhoubolei@gmail.com>
2018-03-17 23:23:07 -0400

    fixed download link

--- models/download.sh
+++ models/download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
-curl -O http://cnnlocalization.csail.mit.edu/demoCAM/models/imagenet_googleletCAM_train_iter_120000.caffemodel
+curl -O http://cnnlocalization.csail.mit.edu/demoCAM/models/imagenet_googlenetCAM_train_iter_120000.caffemodel
